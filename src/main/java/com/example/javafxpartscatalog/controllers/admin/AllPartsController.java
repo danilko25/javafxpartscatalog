23453b9f0559c65dc871c907be2c71cec3141813
Danil <danil.r.savchenko@gmail.com>
2023-05-28 12:52:48 +0300

    Create admin page for list of all parts

---
+++ src/main/java/com/example/javafxpartscatalog/controllers/admin/AllPartsController.java
@@ -0,0 +1,55 @@
+package com.example.javafxpartscatalog.controllers.admin;
+
+import com.example.javafxpartscatalog.dao.PartDAO;
+import com.example.javafxpartscatalog.models.forJavaFxTableView.FXPart;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.TableView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.stream.Collectors;
+
+public class AllPartsController implements Initializable {
+
+    @FXML
+    private TableView<FXPart> partsTable;
+
+    @FXML
+    private TableColumn<FXPart, String> condition;
+
+    @FXML
+    private TableColumn<FXPart, Integer> id;
+
+    @FXML
+    private TableColumn<FXPart, Integer> manufacturerId;
+
+    @FXML
+    private TableColumn<FXPart, String> name;
+
+    @FXML
+    private TableColumn<FXPart, String> originalPartCode;
+
+    @FXML
+    private TableColumn<FXPart, Integer> price;
+
+    private PartDAO partDAO = new PartDAO();
+
+    ObservableList<FXPart> listOfFXParts = FXCollections.observableList(partDAO.getAllParts().stream().map(FXPart::new).collect(Collectors.toList()));
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        id.setCellValueFactory(new PropertyValueFactory<FXPart, Integer>("id"));
+        manufacturerId.setCellValueFactory(new PropertyValueFactory<FXPart, Integer>("manufacturerId"));
+        originalPartCode.setCellValueFactory(new PropertyValueFactory<FXPart, String>("originalPartCode"));
+        name.setCellValueFactory(new PropertyValueFactory<FXPart, String>("name"));
+        condition.setCellValueFactory(new PropertyValueFactory<FXPart, String>("condition"));
+        price.setCellValueFactory(new PropertyValueFactory<FXPart, Integer>("price"));
+
+        partsTable.setItems(listOfFXParts);
+    }
+}
---
+++ src/main/java/com/example/javafxpartscatalog/models/forJavaFxTableView/FXPart.java
@@ -0,0 +1,84 @@
+package com.example.javafxpartscatalog.models.forJavaFxTableView;
+
+import com.example.javafxpartscatalog.enums.PartCondition;
+import com.example.javafxpartscatalog.models.Part;
+
+public class FXPart {
+    private int id;
+    private int manufacturerId;
+    private String originalPartCode;
+    private String name;
+    private String condition;
+    private int price;
+
+    public FXPart() {
+    }
+
+    public FXPart(Part part){
+        this.id = part.getId();
+        this.manufacturerId = part.getManufacturerId();
+        this.originalPartCode = part.getOriginalPartCode();
+        this.name = part.getName();
+        this.condition = part.getCondition().toString();
+        this.price = part.getPrice();
+    }
+
+    public FXPart(int id, int manufacturerId, String originalPartCode, String name, String condition, int price) {
+        this.id = id;
+        this.manufacturerId = manufacturerId;
+        this.originalPartCode = originalPartCode;
+        this.name = name;
+        this.condition = condition;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getManufacturerId() {
+        return manufacturerId;
+    }
+
+    public void setManufacturerId(int manufacturerId) {
+        this.manufacturerId = manufacturerId;
+    }
+
+    public String getOriginalPartCode() {
+        return originalPartCode;
+    }
+
+    public void setOriginalPartCode(String originalPartCode) {
+        this.originalPartCode = originalPartCode;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCondition() {
+        return condition;
+    }
+
+    public void setCondition(String condition) {
+        this.condition = condition;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
+
+
